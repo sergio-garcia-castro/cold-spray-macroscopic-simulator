8b07e28400d78ba90091a88f6d32420c794a2dd0
sergio-garcia-castro <sergio.garcia_castro@etu.minesparis.psl.eu>
2024-10-23 15:30:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# This setup excludes the PyTorch3d library, you will have to install it through pip or Conda.
+# Setup configuration
 setup(
-    name='ColdSpray',
+    name='ColdSprayTest',
     version='0.1.0',
     author='Sergio Garcia',
     author_email='sergio.garcia_castro@etu.minesparis.psl.eu',
     description='Macroscopic simulator of Cold Spray deposition',
+    packages=['Cold_Spray'],
     url='https://github.com/sergio-garcia-castro/cold-spray-macroscopic-simulator/',
     install_requires=["torch", 
         "torchvision", 
