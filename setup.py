0d3cdfabd7d688e7a3e5afe16a193540f6bdb56f
sergio-garcia-castro <sergio.garcia_castro@etu.minesparis.psl.eu>
2024-10-23 15:27:20 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
+# This setup excludes the PyTorch3d library, you will have to install it through pip or Conda.
 setup(
     name='ColdSpray',
-    version='0.1.0',  # Define your package version
-    author='Sergio Garcia',  # Replace with your name
-    author_email='sergio.garcia_castro@etu.minesparis.psl.eu',  # Replace with your email
+    version='0.1.0',
+    author='Sergio Garcia',
+    author_email='sergio.garcia_castro@etu.minesparis.psl.eu',
     description='Macroscopic simulator of Cold Spray deposition',
-    long_description=open('README.md').read(),  # You can create a README.md file for long description
-    long_description_content_type='text/markdown',
-    url='https://github.com/sergio-garcia-castro/cold-spray-macroscopic-simulator/',  # Replace with your project URL
-    packages=['Cold_Spray'],
-    install_requires=["pytorch==1.13.0", "torchvision", "pytorch-cuda==11.6", "iopath", "pytorch3d", "matplotlib"],  # Specify the dependencies
-    python_requires='==3.9.19',  # Specify the required Python version
-)
-
+    url='https://github.com/sergio-garcia-castro/cold-spray-macroscopic-simulator/',
+    install_requires=["torch", 
+        "torchvision", 
+        "torchaudio", 
+        "iopath",
+        "matplotlib"],
+    py_modules=[],
+    python_requires='>=3.9.19')
